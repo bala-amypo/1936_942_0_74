3c9e4ea6e366843bdd2da2ecba8325f830e37251
bala-amypo <bala@amypo.in>
2025-12-17 04:23:32 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
         return new OpenAPI()
                 // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("https://9005.vs.amypo.ai")
+                        new Server().url("https://9102.408procr.amypo.ai/")
                 ));
         }
 }
